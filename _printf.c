34a266b15663653b8731ca7ef4732bcf3e717b4b
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-27 20:26:50 +0000

    testing printf

--- _printf.c
+++ _printf.c
@@ -3,74 +3,60 @@
 #include "main.h"
 
 /**
- * handle_conversion - handles the specifier cases.
- * @l: character to handle;
- * @ptr: Argument list pointer;
- *
- * Return: count of characters printed.
+* _printf - printf function
+* @format: format
+*
+* Return: number of characters printed
 */
-int handle_conversion(char l, va_list ptr)
+int _printf(const char *format, ...)
 {
-	int ext_count = 0, tmp;
+	va_list args;
 
-	if (l == 'c')
+	int i = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		_putchar((char)(va_arg(ptr, int)));
-		ext_count = 1;
-	}
-	else if (l == 's')
-	{
-		ext_count = printstr(va_arg(ptr, char *));
-	}
-	else if (l == '%')
-	{
-		_putchar(l);
-		ext_count = 1;
-	}
-	else if (l == 'd' || l == 'i')
-	{
-		tmp = va_arg(ptr, int);
-		ext_count = countnum(tmp);
-		printnum(tmp);
-	}
+		if (format[i] != '%')
+		{
+			i++;
+
+		switch (format[i])
+		{
+			case 'c':
+			{
+				int x = va_arg(args, int);
+
+				printf("%c", x);
+					break;
+			}
+			case's':
+			{
+				char *x = va_arg(args, char*);
+
+				printf("%s", x);
+				break;
+			}
+			case '%':
+			putchar('%');
+				break;
+			default:
+				putchar(format[i]);
+				break;
+		}
+		}	
 	else
-	{
-		/* do nothing. */
+	{				
+		putchar(format[i]);
+	}	
+		i++;
 	}
-	return (ext_count);
-}
+	
+	va_end(args);
 
-/**
- * _printf - prints/formats a string
- * @str: String with/without specifier to print.
- *
- * Return: Number of characters printed.
-*/
-int _printf(char *str, ...)
-{
-	int count = 0, ext_count;
-	char l;
-	va_list ptr;
-
-	va_start(ptr, str);
-	while (*str && *str != '\0')
-	{
-		if (*str == '%')
-		{
-			str++;
-			l = *str;
-			ext_count = handle_conversion(l, ptr);
-			count += ext_count;
-			str++;
-		}
-		else
-		{
-			_putchar(*str);
-			str++;
-			count++;
-		}
-	}
-	va_end(ptr);
-	_putchar('\0');
-	return (count);
+	return (0);
 }
