fd6ce431688fd4c44bd3264911ed26da74688e42
Daniel Abayomi <dan234bayomi@gmail.com>
2023-03-28 02:02:48 -0700

    Added my previous main.h

---
+++ test/1-main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void);
+int _putchar(char c);
+int printstr(char *str);
+int printnum(int n);
+int countnum(int n);
+int print_c(va_list ptr);
+int print_s(va_list ptr);
+int print_p(va_list ptr);
+int print_i(va_list ptr);
+int _puts(char *s);
+int _printf(const char *format, ...);
+
+/**
+ * struct f_ops - struct that links format to appropriate function
+ * @c: Format specifier string
+ * @f: Pointer to appropriate function.
+ */
+typedef struct f_ops
+{
+	char *c;
+	int (*f)(va_list);
+} f_ops;
+
+#endif
