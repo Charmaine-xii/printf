2969c3c1febfd5a4fea745d0efd7a3ddd25d0a07
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-28 07:16:33 +0000

    testing printf function

---
+++ prinf.c
@@ -0,0 +1,77 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _printf - printf function
+* @format: format
+*
+* Return: number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++; /*skip the % */
+			switch (*format)
+			{
+				case 'c':
+				{
+					char c = va_arg(args, int);
+
+					putchar(c);
+					break;
+				}
+				case 's':
+				{
+					char *s = va_arg(args, char *);
+
+					fputs(s, stdout);
+					break;
+				}
+				case '%':
+				{
+					putchar('%');
+					break;
+				}
+				default:
+				{
+					/* unknown format specifier*/
+					putchar('%');
+					putchar(*format);
+					break;
+				}
+			}
+		} else
+		{
+			putchar(*format);
+		}
+		format++;
+	}
+
+	va_end(args);
+
+	return (0);
+}
+
+/** 
+ * main - entry point 
+ *
+ * Return: 0 (sucessful)
+ */
+int main(void)
+{
+	char c = 'A';
+	char *s = "hello";
+
+	_printf("%c %s %%\n", c, s);
+
+	return (0);
+}
