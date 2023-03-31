1267d5423d6f1d4b8b2c65f6a549a9653196d5cf
Charmaine-xii <charmainemohlala12@gmail.com>
2023-03-31 12:19:11 +0000

    printf update

--- _printf.c
+++ _printf.c
@@ -1,96 +1,77 @@
-##include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
-void printf(char*, ...);
+#include <stdarg.h>
+#include <stddef.h>
 
 /**
-* _printf - print function
-* @format: format
-*
-* Return: number of characters printed
-*/
-int _printf(const char *format, ...)
+ * get_op - select function for conversion char
+ * @c: char to check
+ * Return: pointer to function
+ */
+
+int (*get_op(const char c))(va_list)
 {
-	va_list args;
-	int count = 0;
+	int i = 0;
 
-	va_start(args, format);
-
-	while (*format)
+	flags_p fp[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"i", print_nbr},
+		{"d", print_nbr},
+		{"%", print_percent}
+	};
+	while (i < 14)
 	{
-		if (*format == '%')
+		if (c == fp[i].c[0])
 		{
-			format++;
-
-			switch (*format)
-			{
-				case 'c':
-				{
-					char c = va_arg(args, int);
-
-					putchar(c);
-					count++;
-					break;
-				}
-
-				case 's':
-				{
-					char *s = va_arg(args, char*);
-
-					fputs(s, stdout);
-					count += strlen(s);
-					break;
-				}
-
-				case '%':
-				{
-					putchar('%');
-					count++;
-					break;
-				}
-
-				default:
-
-				{
-					putchar('%');
-					putchar(*format);
-					count += 2;
-					break;
-				}
-			}
+			return (fp[i].f);
 		}
-
-		else
-
-		{
-			putchar(*format);
-			count++;
-		}
-
-		format++;
+		i++;
 	}
-
-	va_end(args);
-
-	return (count);
+	return (NULL);
 }
 
 /**
-* main - entry point
-*
-* Return: 0 (successful)
-*/
-int main(void)
+ * _printf - Reproduce behavior of printf function
+ * @format: format string
+ * Return: value of printed chars
+ */
+
+int _printf(const char *format, ...)
 {
-	char c = 'A';
-	char *s = "hello";
+	va_list ap;
+	int sum = 0, i = 0;
+	int (*func)();
 
-	int count = _printf("%c %s %%\n", c, s);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(ap, format);
 
-	printf("count:%d\n", count);
-
-	return (0);
-
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] != '\0')
+				func = get_op(format[i + 1]);
+			if (func == NULL)
+			{
+				_putchar(format[i]);
+				sum++;
+				i++;
+			}
+			else
+			{
+				sum += func(ap);
+				i += 2;
+				continue;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			sum++;
+			i++;
+		}
+	}
+	va_end(ap);
+	return (sum);
 }
